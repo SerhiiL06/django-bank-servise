4a11f17e52e782db91f2ac6ec69e79062cca17ae
SerhiiL06 <142268536+SerhiiL06@users.noreply.github.com>
2023-11-17 19:08:22 +0200

    initialization app with basic user system

---
+++ users/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager,
+    AbstractBaseUser,
+    PermissionsMixin,
+)
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class CustomUserManager(BaseUserManager):
+    def _create_user(self, phone_number, email, password, **extra_fields):
+        if not phone_number:
+            raise ValueError("You need to enter your phone numer")
+
+        email = self.normalize_email(email)
+
+        user = self.model(phone_number=phone_number, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_user(self, email, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, **extra_fields)
+
+    def create_superuser(self, phone_number, email, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self._create_user(phone_number, email, **extra_fields)
+
+
+class User(PermissionsMixin, AbstractBaseUser):
+    email = models.EmailField(unique=True, max_length=255)
+    phone_number = PhoneNumberField(region="UA", unique=True)
+    city = models.CharField(max_length=50)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField()
+    is_superuser = models.BooleanField()
+
+    join_at = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = "phone_number"
+    EMAIL_FIELD = "email"
+
+    REQUIRED_FIELDS = ["email"]
+
+    objects = CustomUserManager()
